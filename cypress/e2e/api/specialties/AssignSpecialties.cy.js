5ea7294ccbc7a090cf213f431e2e85f2d7d21b92
bartoszkolodziej <bartosz.kolodziej@comeon.com>
2024-04-27 19:07:39 +0200

    adding tests for assing specialties UI using API request

--- cypress/e2e/api/specialties/AssignSpecialties.cy.js
+++ cypress/e2e/api/specialties/AssignSpecialties.cy.js
@@ -1,7 +1,7 @@
 import {getRandomDoctor} from "../../../generator/userGenerator";
 import {register} from "../../../requests/register.api";
 import {login} from "../../../requests/login.api";
-import {getAllSpeciality} from "../../../requests/specialities.api";
+import {assignSpeciality, getAllSpeciality} from "../../../requests/specialities.api";
 import {drawTwoRandomIds} from "../../../utils/ranodm.array";
 
 describe('Assign specialties', () => {
@@ -17,15 +17,7 @@ describe('Assign specialties', () => {
             const ids = response.body.map(specialty => specialty.id)
             const randomIds = drawTwoRandomIds(ids)
 
-            cy.api({
-                method: 'PUT',
-                url: 'http://localhost:4001/users/specialties',
-                body: {
-                    specialtyIds: randomIds
-                }
-            }).then((response) => {
-                expect(response.status).to.eq(200);
-            })
+            assignSpeciality(randomIds);
         })
     })
 })
